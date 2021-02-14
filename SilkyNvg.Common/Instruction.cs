7a5a88e17d4a8e81e7cb170233f9d2d647c1827c
pseinche <philipp.seincher@smail.th-koeln.de>
2021-02-14 21:05:11 +0100

    Added + operator to combine Instructions

--- SilkyNvg.Common/Instruction.cs
+++ SilkyNvg.Common/Instruction.cs
@@ -6,7 +6,9 @@ namespace SilkyNvg.Common
 {
     public sealed class Instruction
     {
-        private float[] todo;
+        Queue<float> sequence = new Queue<float>();
+
+
 
         public static Instruction Move(float x, float y)
         {
@@ -31,6 +33,22 @@ namespace SilkyNvg.Common
         public static Instruction Winding()
         {
             return null;
+        }
+
+        //Merges two Queues of the Instructions
+        public static Instruction operator +(Instruction instruction_a, Instruction instruction_b)
+        {
+            Instruction new_instruction = new Instruction();
+            float[] array_a = instruction_a.sequence.ToArray();
+            float[] array_b = instruction_b.sequence.ToArray();
+
+            float[] combinedArray = new float[array_a.Length + array_b.Length];
+            Array.Copy(array_a, combinedArray, array_a.Length);
+            Array.Copy(array_b, 0, combinedArray, array_a.Length, array_b.Length);
+
+            new_instruction.sequence = new Queue<float>(combinedArray);
+
+            return new_instruction;
         }
     }
 }
