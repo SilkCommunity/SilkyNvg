395c3493cde28942cd3d3ecdaff8e3e030aa5382
59365069 <matija.brown@outlook.de>
2021-02-14 20:30:19 +0100

    Point flags

---
+++ src/SilkyNvg.Core/Geometry/Point.cs
@@ -0,0 +1,47 @@
+﻿using Silk.NET.Maths;
+
+namespace SilkyNvg.Core.Geometry
+{
+    public class Point
+    {
+
+        private readonly Vector3D<float> _position;
+        private readonly uint _flags;
+
+        public float X => _position.X;
+        public float Y => _position.Y;
+        public float Z => _position.Z;
+        public Vector3D<float> Position => _position;
+
+        public uint Flags => _flags;
+
+        public Point(float x, float y, float z, uint flags) : this(new Vector3D<float>(x, y, z), flags) { }
+
+        public Point(Vector3D<float> position, uint flags)
+        {
+            _position = position;
+            _flags = flags;
+        }
+
+        public bool IsCorner()
+        {
+            return (_flags & (uint)PointFlags.PointCorner) != 0;
+        }
+
+        public bool IsLeft()
+        {
+            return (_flags & (uint)PointFlags.PointLeft) != 0;
+        }
+
+        public bool IsBevel()
+        {
+            return (_flags & (uint)PointFlags.PointBevel) != 0;
+        }
+
+        public bool IsInnerbevel()
+        {
+            return (_flags & (uint)PointFlags.PointInnerbevel) != 0;
+        }
+
+    }
+}
---
+++ src/SilkyNvg.Core/Geometry/PointFlags.cs
@@ -0,0 +1,12 @@
+﻿namespace SilkyNvg.Core.Geometry
+{
+    public enum PointFlags
+    {
+
+        PointCorner = 1 << 0,
+        PointLeft = 1 << 1,
+        PointBevel = 1 << 2,
+        PointInnerbevel = 1 << 3
+
+    }
+}
