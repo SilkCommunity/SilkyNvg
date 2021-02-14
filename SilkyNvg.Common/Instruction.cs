405e892a95806e145529f5a57a40c4d5b6897b7b
pseinche <philipp.seincher@smail.th-koeln.de>
2021-02-14 20:50:48 +0100

    Added Class For Lines, Shapes and Instructions

---
+++ SilkyNvg.Common/Instruction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SilkyNvg.Common
+{
+    public sealed class Instruction
+    {
+        private float[] todo;
+
+        public static Instruction Move(float x, float y)
+        {
+            return null;
+        }
+
+        public static Instruction Line(float x, float y)
+        {
+            return null;
+        }
+
+        public static Instruction Bezier(float x, float y)
+        {
+            return null;
+        }
+
+        public static Instruction Close()
+        {
+            return null;
+        }
+
+        public static Instruction Winding()
+        {
+            return null;
+        }
+    }
+}
---
+++ SilkyNvg.Common/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SilkyNvg.Common
+{
+    public static class Shape
+    {
+
+        //Return Commands instead of void?
+        public static void Ellipse(float cx, float cy, float rx, float ry)
+        {
+
+        }
+
+        //Return Commands instead of void?
+        public static void Circle(float cx, float cy, float r)
+        {
+
+        }
+
+    }
+}
