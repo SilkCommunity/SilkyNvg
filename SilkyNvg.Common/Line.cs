98d1602786833106d1cca073735182e06b30606b
pseinche <philipp.seincher@smail.th-koeln.de>
2021-02-15 02:20:23 +0100

    Added temporal Classes for lines and shapes, sry when code is gaga, its late and so am I

--- SilkyNvg.Common/Line.cs
+++ SilkyNvg.Common/Line.cs
@@ -4,8 +4,20 @@ using System.Text;
 
 namespace SilkyNvg.Common
 {
-    class Line
+    public static class Line
     {
+        public static Instruction StraightLine(float x, float y)
+        {
+            Instruction instruction = new Instruction();
+            instruction.Queue( Instruction.LINE, x, y);
+            return instruction;
+        }
 
+        public static Instruction BezierLine(float x1, float y1, float x2, float y2, float x3, float y3)
+        {
+            Instruction instruction = new Instruction();
+            instruction.Queue( Instruction.BEZIER, x1, y1, x2, y2, x3, y3);
+            return instruction;
+        }
     }
 }
