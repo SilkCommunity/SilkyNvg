fd277abcedd9ac499d7d612470c05b368499c70c
59365069 <matija.brown@outlook.de>
2025-03-07 17:09:14 +0100

    Removed vulkan renderer code

--- src/rendering/SilkyNvg.Rendering.Vulkan/Shaders/compileShaders.bat
+++
@@ -1,7 +0,0 @@
-glslangValidator.exe -V nanovg_vertexShader.vert
-rename vert.spv nanovg_vertexShader.spv
-glslangValidator.exe -V nanovg_fragmentShader.frag
-rename frag.spv nanovg_fragmentShader.spv
-glslangValidator.exe -V nanovg_fragmentShaderEdgeAA.frag
-rename frag.spv nanovg_fragmentShaderEdgeAA.spv
-pause
