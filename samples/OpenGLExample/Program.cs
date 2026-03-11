17a1bdc4708a74ed36b662c8c6ab2470f77683b1
MatijaBrown <matija.brown@outlook.de>
2026-03-11 22:32:28 +0100

    Simple OPenGL for example

--- samples/OpenGLExample/Program.cs
+++ samples/OpenGLExample/Program.cs
@@ -1,25 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Silk.NET.Maths;
+using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
 
-var options = WindowOptions.Default;
-options.Title = "OpenGL Example";
-options.Size = new Vector2D<int>(1280, 720);
+namespace OpenGLExample;
 
-var window = Window.Create(options);
-window.Update += Update;
-window.Render += Render;
-window.Run();
-
-return;
-
-static void Update(double delta)
+internal class Program : IDisposable
 {
     
-}
-
-static void Render(double _)
-{
+    private readonly IWindow _window;
     
+    private GL _gl;
+
+    private Program(IWindow window)
+    {
+        _window = window;
+
+        _window.Load += Load;
+        _window.Resize += Resize;
+        _window.Update += Update;
+        _window.Render += Render;
+        _window.Closing += Close;
+    }
+
+    private void Load()
+    {
+        _gl = GL.GetApi(_window);
+
+        Resize(_window.Size);
+    }
+
+    private void Resize(Vector2D<int> newSize)
+    {
+        _gl.Viewport(newSize);
+    }
+
+    private void Update(double delta)
+    {
+        
+    }
+
+    private void Render(double _)
+    {
+        _gl.ClearColor(0.2f, 0.3f, 0.4f, 1.0f);
+        _gl.Clear(ClearBufferMask.ColorBufferBit);
+    }
+
+    private void Close()
+    {
+        _gl.Dispose();
+    }
+
+    public void Dispose()
+    {
+        
+    }
+    
+    static void Main(string[] args)
+    {
+        var options = WindowOptions.Default;
+        options.Size = new Vector2D<int>(1280, 720);
+        options.Title = "OpenGL Example";
+
+        var window = Window.Create(options);
+        var instance = new Program(window);
+
+        window.Run();
+
+        instance.Dispose();
+        window.Dispose();
+    }
 }
